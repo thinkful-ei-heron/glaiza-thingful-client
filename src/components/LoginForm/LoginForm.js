1701cccb1ded5395fa27cfd4acd9c9b60460efc6
Glaiza Wagner <glaizawagner@gmail.com>
2019-11-15 14:22:51 -0700

    finished

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
 import { Button, Input } from '../Utils/Utils'
 
 export default class LoginForm extends Component {
@@ -13,8 +14,6 @@ export default class LoginForm extends Component {
     ev.preventDefault()
     const { user_name, password } = ev.target
 
-    // console.log('login form submitted')
-    // console.log({ user_name, password })
 
     TokenService.saveAuthToken( 
       TokenService.makeBasicAuthToken(user_name.value, password.value)
@@ -25,12 +24,33 @@ export default class LoginForm extends Component {
     this.props.onLoginSuccess()
   }
 
+  handleSubmitJwtAuth = ev => {
+      ev.preventDefault()
+      this.setState({ error: null })
+      const { user_name, password } = ev.target
+    
+      AuthApiService.postLogin({
+        user_name: user_name.value,
+        password: password.value,
+      })
+        .then(res => {
+          user_name.value = ''
+          password.value = ''
+          TokenService.saveAuthToken(res.authToken)
+          this.props.onLoginSuccess()
+        })
+        .catch(res => {
+          this.setState({ error: res.error })
+        })
+  }
+
   render() {
     const { error } = this.state
     return (
       <form
         className='LoginForm'
-        onSubmit={this.handleSubmitBasicAuth}
+        // onSubmit={this.handleSubmitBasicAuth}
+        onSubmit={this.handleSubmitJwtAuth}
       >
         <div role='alert'>
           {error && <p className='red'>{error}</p>}
