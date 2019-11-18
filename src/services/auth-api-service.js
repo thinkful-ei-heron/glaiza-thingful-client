de0f509bcef06c7fba06f0ad2353cea6973b77c9
Glaiza Wagner <glaizawagner@gmail.com>
2019-11-17 17:43:57 -0700

    for user registration updates

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,21 +1,6 @@
 import config from '../config'
 
 const AuthApiService = {
-  postLogin(credentials) {
-    return fetch(`${config.API_ENDPOINT}/auth/login`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
-  },
-
   postUser(user) {
       return fetch(`${config.API_ENDPOINT}/users`, {
         method: 'POST',
@@ -29,6 +14,20 @@ const AuthApiService = {
             ? res.json().then(e => Promise.reject(e))
             : res.json()
         )
+  },
+  postLogin(credentials) {
+    return fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
   },
   
 }
